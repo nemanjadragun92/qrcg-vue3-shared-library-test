399c4568f881bb760b284024854e0088453fc542
nemanjadragun <nemanja@egoditor.com>
2022-08-30 12:20:02 +0200

    feat: migrate storybook config

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,22 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  kindleFire2: {
+    name: 'Nest Hub',
+    styles: {
+      width: '1024px',
+      height: '600px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Nest Hub Max',
+    styles: {
+      width: '1280px',
+      height: '800px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +24,24 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...customViewports
+    },
+  },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#fdfeff',
+      },
+      {
+        name: 'dark',
+        value: '#1b294b',
+      },
+    ],
   },
 }
